ae584623260c033df69d7e80263d693a710e5b08
eyodohan <eyodohan@gmail.com>
2022-01-17 12:36:09 +0300

    local storage

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Model } from '../model';
+import { TodoItem } from '../todoItem';
 
 @Component({
   selector: 'app-todo',
@@ -8,26 +9,50 @@ import { Model } from '../model';
 })
 export class TodoComponent  {
 
-  constructor() { }
+  constructor() {
+    this.model.items = this.getItemsFromLS()
+   }
   
   model = new Model()
 
   displayAll:boolean = false
 
-  toggleAction(x:boolean){
-    return !x
-  }
-
   inputText:string = ""
   addItem(){
+    let data = {description:this.inputText,action: false}
+    let items = this.getItemsFromLS()
+    items.push(data)
     if(this.inputText!=""){ 
-      this.model.items.push( {description:this.inputText,action: false});
+      this.model.items.push(data);
+      localStorage.setItem("items",JSON.stringify(items))
       this.inputText = ""
     }else{
       alert("Bilgi giriniz!!!")
     }
   }
 
+  getItemsFromLS(){
+    let items:TodoItem[] = []
+    let value = localStorage.getItem("items")
+    if(value !=null){
+      items = JSON.parse(value)
+    }
+    return items
+  }
+
+  onActionChanged(item:TodoItem){
+    // console.log(item)
+    let items = this.getItemsFromLS()
+
+    localStorage.clear()
+
+    items.forEach(i=>{
+      if(i.description == item.description){
+        i.action = item.action
+      }
+    })
+    localStorage.setItem("items",JSON.stringify(items))
+  }
 
  getName(){
    return this.model.name
